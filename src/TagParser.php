548c00392e4c4f71ea6747a25abe24f80041004a
Halawany <mohamed.mostafa98@outlook.com>
2021-12-07 00:00:22 +0200

    use more cleaner way to write testcase

--- src/TagParser.php
+++ src/TagParser.php
@@ -5,6 +5,6 @@ namespace App;
 class TagParser {
     public static function parse(string $string) : array
     {
-        return preg_split('/ ?[,|] ?/', $string);
+        return preg_split('/ ?[,|!] ?/', $string);
     }
 }
--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -7,37 +7,24 @@ use App\TagParser;
 
 class TagParserTest extends TestCase{
 
-    private $expected = ['PHP', 'C#', 'R', 'Python'];
-
-    public function test_it_parse_comma_separeted_string()
+    /**
+     * @dataProvider tagsProvider
+     */
+    public function test_it_can_parse($input, $expected)
     {
-        $string = 'PHP,C#,R,Python';
-        $result = TagParser::parse($string);
-
-        $this->assertSame($this->expected, $result);
+        $result = TagParser::parse($input);
+        $this->assertSame($expected, $result);
     }
 
-    public function test_it_parse_space_comma_with_space_separeted_string()
+    public function tagsProvider()
     {
-        $string = 'PHP, C#, R, Python';
-        $result = TagParser::parse($string);
+        return [
+            ['PHP,C#,R,Python', ['PHP', 'C#', 'R', 'Python']],
+            ['PHP, C#, R, Python', ['PHP', 'C#', 'R', 'Python']],
+            ['PHP|C#|R|Python', ['PHP', 'C#', 'R', 'Python']],
+            ['PHP | C# | R | Python', ['PHP', 'C#', 'R', 'Python']],
+            ['PHP ! C# ! R ! Python', ['PHP', 'C#', 'R', 'Python']],
 
-        $this->assertSame($this->expected, $result);
-    }
-
-    public function test_it_parse_pipe_seperated_string()
-    {
-        $string = 'PHP|C#|R|Python';
-        $result = TagParser::parse($string);
-
-        $this->assertSame($this->expected, $result);
-    }
-
-    public function test_it_parse_pipe_with_space_seperated_string()
-    {
-        $string = 'PHP | C# | R | Python';
-        $result = TagParser::parse($string);
-
-        $this->assertSame($this->expected, $result);
+        ];
     }
 }
